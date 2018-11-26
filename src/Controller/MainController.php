ffd2fa463cea93cd7984fd76373d804ea3f296a5
Patrick Morel <delasalle.sio.morel.p@gmail.com>
2018-11-26 15:59:00 +0100

    Add login and register

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,6 +16,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends Controller
 {
@@ -25,6 +26,27 @@ class MainController extends Controller
     public function home()
     {
         return $this->render("main/home.html.twig");
+    }
+
+//    public function login(AuthenticationUtils $authenticationUtils)
+//    {
+//        // get the login error if there is one
+//        $error = $authenticationUtils->getLastAuthenticationError();
+//
+//        // last username entered by the user
+//        $lastUsername = $authenticationUtils->getLastUsername();
+//
+//        return $this->render('main/login.html.twig', array(
+//            'last_username' => $lastUsername,
+//            'error'         => $error,
+//        ));
+//    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(){
+        return $this->render("main/login.html.twig");
     }
 
 }
