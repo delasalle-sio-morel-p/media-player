70545228b46cd465c14e1e48bacc109483c82fbd
mbourvic <40562990+mbourvic@users.noreply.github.com>
2018-11-26 14:55:01 +0100

    login interface

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,15 +1,35 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: mbourvic2017
- * Date: 26/11/2018
- * Time: 14:18
+ * User: lbouvet
+ * Date: 19/11/2018
+ * Time: 11:23
  */
 
 namespace App\Controller;
 
 
-class AdminController
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends Controller
 {
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return $this->render("admin/index.html.twig");
+    }
 
 }
+
+
+
+
+
